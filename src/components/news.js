5fd306600d920bbc5996636fe4bbf3c2d6f890a4
Chatdvph15765 <chatdvph15765@fpt.edu.vn>
2022-02-22 15:46:07 +0700

    update

--- src/components/news.js
+++ src/components/news.js
@@ -1,10 +1,8 @@
 import { getAll } from "../api/post";
-
 const News = {
     async render() {
         const { data } = await getAll();
         return /* html */ `
-                    <h2 class="font-semibold text-blue-900 my-4 uppercase text-2xl">Tin tức học tập</h2>
                     <div class="grid grid-cols-3 gap-8">
                         ${data.map((post) => `
                         <div class="border p-3">
--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,15 +1,74 @@
 import { get } from "../api/post";
-
+import Footer from "../components/footer";
+import Header from "../components/header";
 const DetailPage = {
     async render(id) {
         const { data } = await get(id);
         // const result = data.find((post) => post.id === id);
         return /* html */`
-            <div>
-                <h1>${data.title}</h1>
-                <img src="${data.img}" alt="" />
-                <p>${data.desc}</p>
+            ${Header.render()}
+            <section class="product">
+            <div class="product-top row">
+                    <p>Trang chủ</p> <span>&#8594;</span>
+                    <p>Chi tiết sản phẩm</p><span></span>
+                </div>
+            <div class="container">
+                <div class="product-content row">
+                    <div class="product-content-left row">
+                        <div class="product-content-left-big-img">
+                            <img src="${data.img}" alt="">
+                        </div>                       
+                    </div>
+                    <div class="product-content-right">
+                        <div class="product-content-right-name">
+                            <h1>${data.title}</h1>
+                        </div>
+                        <div class="product-content-right-price">
+                            <p>${data.price}<sup>đ</sup></p>
+                        </div>
+                        <div class="quantity">
+                            <p style="font-weight: bold;">Số lượng:</p>
+                            <input type="number" min="0" value="1">
+                        </div>
+                        <p style="color:red;">Vui lòng chọn size*</p>
+                        <div class="product-content-right-button">
+                            <button><i class="fas fa-cart-plus"></i>
+                                <p>MUA HÀNG</p>
+                            </button>
+                        </div>
+                        <div class="product-content-right-icon">
+                            <div class="product-content-right-icon-item">
+                                <i class="fas fa-phone"></i>
+                                <p>Hotline</p>
+                            </div>
+                            <div class="product-content-right-icon-item">
+                                <i class="fas fa-comments"></i>
+                                <p>Comments</p>
+                            </div>
+                            <div class="product-content-right-icon-item">
+                                <i class="fas fa-envelope"></i>
+                                <p>Mail</p>
+                            </div>
+                        </div>
+                        <div class="product-content-right-qr">
+                            <img src="./img/qrcode2.png" alt="">
+                        </div>
+                        <div class="product-content-right-bottom">
+                            <div class="product-content-right-bottom-top">
+                                &#8744;
+                            </div>
+                            <div class="product-content-right-bottom-big">
+                                <div class="product-content-right-bottom-title">
+                                    <p>${data.desc}</p> 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </section>
+        ${Footer.render()}
+
         `;
     },
 };
